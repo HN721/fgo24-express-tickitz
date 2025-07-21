e6bde3888f197b11b82b692624c289884ae8f54b
HN721 <hosea1422@gmail.com>
2025-07-21 16:26:11 +0700

    feat(route): add admin-only route to get all transactions with details

--- src/routers/transaction.router.js
+++ src/routers/transaction.router.js
@@ -1,6 +1,14 @@
 const transactionRoute = require("express").Router();
 const transactionController = require("../controller/transaction.controller");
 const authMiddleware = require("../middleware/authMiddleware");
+const isAdmin = require("../middleware/adminMiddleware");
+
 transactionRoute.post("/", authMiddleware, transactionController.createTrans);
 transactionRoute.get("/", transactionController.getTransactionByUserId);
+transactionRoute.get(
+  "/details",
+  isAdmin,
+  transactionController.getAllTransaction
+);
+
 module.exports = transactionRoute;
